ab9896d5fb4b9123fb1585c9c431c18aaa0da0fd
Akilan M <akilanm2005@gmail.com>
2026-05-01 15:47:55 +0530

    Add files via upload

---
+++ acs712.h
@@ -0,0 +1,18 @@
+/*
+ * acs712.h
+ *
+ *  Created on: Apr 15, 2023
+ *      Author: hussamaldean
+ */
+
+#ifndef ACS712_H_
+#define ACS712_H_
+
+#include "stdint.h"
+
+void acs712_init();
+float get_acs712_current();
+
+
+
+#endif /* ACS712_H_ */
---
+++ uart.h
@@ -0,0 +1,20 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jan 2, 2022
+ *      Author: hussam
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <stdint.h>
+
+#include "stm32f4xx.h"
+
+void uart2_tx_init(uint32_t baud,uint32_t freq);
+char uart2_read(void);
+void uart2_rxtx_init(uint32_t baud,uint32_t freq);
+
+
+#endif /* UART_H_ */
